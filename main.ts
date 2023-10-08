627b596c8a458b786f39ae0037484d3155d114e9
Alcides <acremonezi@gmail.com>
2023-10-08 16:51:48 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 pins.onPulsed(DigitalPin.P16, PulseValue.High, function () {
-    music.play(music.tonePlayable(523, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.UntilDone)
     basic.showString("L")
     carGear = -1
 })
@@ -9,17 +9,21 @@ input.onButtonPressed(Button.A, function () {
         carGear += -1
         basic.showString("" + (carGear))
     } else {
-        basic.showString("" + (carGear))
+    	
     }
 })
 input.onGesture(Gesture.LogoUp, function () {
+    music.stopMelody(MelodyStopOptions.All)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpDown), music.PlaybackMode.InBackground)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
+})
+input.onButtonPressed(Button.AB, function () {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Birthday), music.PlaybackMode.InBackground)
 })
 input.onButtonPressed(Button.B, function () {
     if (carGear <= 8) {
@@ -31,26 +35,17 @@ input.onButtonPressed(Button.B, function () {
     }
 })
 input.onGesture(Gesture.TiltRight, function () {
-    music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpUp), music.PlaybackMode.InBackground)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 1)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    music.play(music.createSoundExpression(
-    WaveShape.Square,
-    5000,
-    0,
-    255,
-    0,
-    500,
-    SoundExpressionEffect.None,
-    InterpolationCurve.Linear
-    ), music.PlaybackMode.UntilDone)
+    music.play(music.createSoundExpression(WaveShape.Square, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 })
 let carGear = 0
 carGear = 0
 basic.showString("" + (carGear))
-pins.setPull(DigitalPin.P16, PinPullMode.PullDown)
+pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P13, PinPullMode.PullDown)
 pins.setPull(DigitalPin.P14, PinPullMode.PullDown)
 basic.forever(function () {
@@ -66,7 +61,7 @@ basic.forever(function () {
             carGear += -1
             basic.showString("" + (carGear))
         } else {
-            basic.showString("" + (carGear))
+        	
         }
     }
     if (pins.map(
